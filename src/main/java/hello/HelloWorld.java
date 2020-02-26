d00d4684c0d7ec94dc22e24fdf1f81588a7d24be
ayerswj <ayerswjackson@gmail.com>
2020-02-26 17:42:44 -0600

    added comment and changed gradle version

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,16 +3,16 @@ package hello;
 import org.joda.time.LocalTime;
 
 // Add a comment
+// Adding a comment weeeeeeeeee by Jackson Ayers
 
-public class HelloWorld 
+public class HelloWorld
 {
 	/* master comment */
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
-    
+
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
 }
-
